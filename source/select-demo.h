26290f2ebce6e245789fed617550a9802346e5d1
RiosQ <joansriozz@gmail.com>
2018-06-06 18:51:07 -0500

    Update select-demo.h

--- source/select-demo.h
+++ source/select-demo.h
@@ -5,7 +5,8 @@
 #define         DEMO_DFROBOT    2
 #define         DEMO_CO2_CAL    3
 #define         DEMO_DUST       4
-#define         DEMO_TEST       5
+#define         DEMO_SPEC       5
+#define         DEMO_TEST       6
 
 #define         DEMO            DEMO_CO2_CAL
 
