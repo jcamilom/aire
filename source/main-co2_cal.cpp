2fbaabca4191078d3e4651ad9e0b414bc24dc3ac
RiosQ <joansriozz@gmail.com>
2018-06-26 23:27:29 -0500

    Add files via upload

--- source/main-co2_cal.cpp
+++ source/main-co2_cal.cpp
@@ -32,29 +32,27 @@ int main(void) {
     ss << voltage;
     std::string val(ss.str());
 
-    // Create a Sentilo server
-    SentiloServer sServer = {"sistemic.udea.edu.co:9091"};    
+    // Creates a Sentilo server
+    SentiloServer server("address");
 
-    // Create a provider
-    Provider provider = {"udeaProvider", "f4b92b05becd3b0b4d894ed78ffd468126a2622011a5a5aaf3165119c24431b7"};
-    
+    // Creates a provider
+    Provider provider("providerName", "theToken", &server);
+
     // Create a sensor's array
-    Sensor sensors[3] = {Sensor("spec"),Sensor("temp"),Sensor("hum")};
-    sensors[0].setValue("0.0");
-    sensors[1].setValue("5.0");
-    sensors[2].setValue("10.0");
-    /* for(int i = 0; i < 1; i++) {
-	//	sensors[i].setID("sensor" + std::to_string(i));
-        sensors[i].setValue(std::to_string(i));
-	} */
+    Sensor sensors[1];
+    for(int i = 0; i < 1; i++) {
+		//sensors[i].setID("sensor" + std::to_string(i));
+        sensors[i].setID("sensorName");
+        sensors[i].setValue(val);
+	}
 
     Sensor *pSensors;
     pSensors = sensors;
 
-    // Create a component for the air measuring system
-    Component airComponent ("componentName", sServer, provider, pSensors, 3);
+    // Create a component
+    Component component("componentName", &provider, pSensors);
 
-    int rt = airComponent.sendSensorsObservations();    
+    int rt = component.sendSensorObservation(0);
     if(rt == 1) {
         rLED = 0;
         bLED = 1;
@@ -62,7 +60,7 @@ int main(void) {
         grLED = 0;
         bLED = 1;
     }
-    printf("URL: %d\r\n", rt);    
+    printf("URL: %d\r\n", rt);
 
     wait(osWaitForever);
 }
--- source/sentilo.h
+++ source/sentilo.h
@@ -13,16 +13,34 @@ void dump_response(HttpResponse* res) {
     printf("\r\nBody (%d bytes):\r\n\r\n%s\r\n", res->get_body_length(), res->get_body_as_string().c_str());
 }
 
-// A struct for the SenstiloServer object
-typedef struct SentiloServerSt {
-    std::string address;        // The Sentilo server address
-} SentiloServer;
+class SentiloServer {
+        std::string address;         // The Sentilo server address
+    public:
+        SentiloServer(std::string add);
+        std::string getAddress() { return address;}
+};
 
-// A struct for the Provider object
-typedef struct ProviderSt {
-    std::string id;             // Provider's id
-    std::string token;          // Authorization token
-} Provider;
+// Member functions definitions including constructor
+SentiloServer::SentiloServer(std::string add) {
+    address = add;
+}
+
+class Provider {
+        std::string id;             // Provider's id
+        std::string token;          // Authorization token
+        SentiloServer *pServer;     // The Sentilo server
+    public:
+        Provider(std::string idArg, std::string tokenArg, SentiloServer *pServerArg);
+        std::string getServerAddress() { return pServer->getAddress();}
+        std::string getID() { return id;}
+        std::string getToken() { return token;}
+};
+
+Provider::Provider(std::string idArg, std::string tokenArg, SentiloServer *pServerArg) {
+    id = idArg;
+    token = tokenArg;
+    pServer = pServerArg;
+}
 
 // This class is intended to get actual values from the sensors and return then to the Component class
 class Sensor {
@@ -30,50 +48,39 @@ class Sensor {
         std::string lastValue;
         bool lastValueOk;
     public:
-        Sensor(std::string idArg): id(idArg) {
-            lastValueOk = false; // TODO: inline???
-        }
-
         std::string getValue() {
             return lastValue;
         }
 
         void setValue(std::string lastValueArg) {
             lastValue = lastValueArg;
-            lastValueOk = true;
         }
 
         bool lastValueOK() {
             return lastValueOk;
         }
 
-        void setLastValueErr() {
-            lastValueOk = false;
+        void setID(std::string idArg) {
+            id = idArg;
         }
 
         std::string getID() { return id;}
-};
+};  
 
 class Component {
     public:
-        std::string id;                 // Component's id
-        SentiloServer sentiloServer;    // The sentiloServer member
-        Provider provider;              // The provider member
-        Sensor *pSensors;               // Pointer to the first element of the array of sensors
-                                        // for this component.
-        int nSensors;                   // Number of sensors pointed by pSensors
+        std::string id;             // Component's id
+        Provider *pProvider;        // Pointer to the related provider
+        Sensor *pSensors;           // Pointer to the first element of the array of sensors
+                                    // for this component.
 
-        Component(std::string idArg, SentiloServer &sentiloServerArg, Provider &providerArg, Sensor *pSensorsArg, int nSensorsArg) {
+        Component(std::string idArg, Provider *pProviderArg, Sensor *pSensorsArg) {
             id = idArg;
-            sentiloServer = sentiloServerArg;
-            provider = providerArg;
+            pProvider = pProviderArg;
             pSensors = pSensorsArg;
-            nSensors = nSensorsArg;
         }
-        
+
         int sendSensorObservation(int idx);
-        int sendSensorObservation(int idx, NetworkInterface* netif);
-        int sendSensorsObservations(void);
 };
 
 /**
@@ -86,7 +93,7 @@ class Component {
 */
 int Component::sendSensorObservation(int idx) {
     // Build the URL Request
-    std::string reqURL (sentiloServer.address + "/data/" + provider.id + "/" + (pSensors + idx)->getID() + "/" + (pSensors + idx)->getValue());
+    std::string reqURL (pProvider->getServerAddress() + "/data/" + pProvider->getID() + "/" + (pSensors + idx)->getID() + "/" + (pSensors + idx)->getValue());
 
     // EthernetInterface object
     EthernetInterface eth;
@@ -104,7 +111,7 @@ int Component::sendSensorObservation(int idx) {
     {
         HttpRequest* put_req = new HttpRequest(netif, HTTP_PUT, reqURL.c_str());
         //put_req->set_header("Content-Type", "application/json");
-        put_req->set_header("IDENTITY_KEY", provider.token.c_str());
+        put_req->set_header("IDENTITY_KEY", pProvider->getToken().c_str());
 
         //const char body[] = "{\"hello\":\"world\"}";
 
@@ -119,76 +126,5 @@ int Component::sendSensorObservation(int idx) {
 
         delete put_req;
     }
-    return 0;
-}
-
-/**
-* Send a sensor's observation to the Sentilo platform.
-*
-*
-*
-* @param[in]
-* @param[in]
-*/
-int Component::sendSensorObservation(int idx, NetworkInterface* netif) {
-    // Build the URL Request
-    std::string reqURL (sentiloServer.address + "/data/" + provider.id + "/" + (pSensors + idx)->getID() + "/" + (pSensors + idx)->getValue());
-
-    // PUT request to publish an observabtion on Sentilo
-    {
-        HttpRequest* put_req = new HttpRequest(netif, HTTP_PUT, reqURL.c_str());
-        //put_req->set_header("Content-Type", "application/json");
-        put_req->set_header("IDENTITY_KEY", provider.token.c_str());
-
-        //const char body[] = "{\"hello\":\"world\"}";
-
-        HttpResponse* put_res = put_req->send();//send(body, strlen(body));
-        if (!put_res) {
-            printf("HttpRequest failed (error code %d)\r\n", put_req->get_error());
-            return 1;
-        }
-
-        printf("\r\n----- HTTP PUT response -----\r\n");
-        dump_response(put_res);
-
-        delete put_req;
-    }
-    return 0;
-}
-
-/**
-* Send all sensor's observations to the Sentilo platform.
-*
-*
-*
-* @param[in]
-* @param[in]
-*/
-int Component::sendSensorsObservations(void) {
-    // TODO: check connection and return error values.
-    // If connection ok start sending observations.
-    // Perhaps macros need to be defined.
-
-    // EthernetInterface object
-    EthernetInterface eth;
-
-    NetworkInterface* netif = &eth;
-    int resp_success = eth.connect();
-    if(resp_success == 0) {
-        printf("[Network] Connected to Network successfully\r\n");
-    } else {
-        printf("[Network] Connection to Network Failed %d!\r\n", resp_success);
-        return 1;
-    }
-
-    for(int i = 0; i < nSensors; i++) {
-		if((pSensors + i)->lastValueOK()) {
-            // If status ok, send data
-            resp_success = sendSensorObservation(i, netif);
-            if(resp_success == 1) return 1;
-        } else {
-            // TODO: send error value
-        }
-	}
-    return 0;
+    return 0;    
 }
