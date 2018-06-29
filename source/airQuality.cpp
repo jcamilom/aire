975b438a5f38b3f76d828b66ca6b7235d1db24e2
RiosQ <joansriozz@gmail.com>
2018-06-29 16:48:30 -0500

    shinyei & SPEC working

--- source/airQuality.cpp
+++ source/airQuality.cpp
@@ -4,12 +4,37 @@
 
 #include "airQuality.h"
 
-// Serial communication
-Serial pc(USBTX, USBRX);        // To communicate with the PC
+//General
+long samples = 0;
 
 // EthernetInterface object
 EthernetInterface eth;
 NetworkInterface* netif = &eth;
+int counter;
+int ack = 0;
+
+// K64F terminals
+SDBlockDevice bd(PTE3, PTE1, PTE2, PTE4);
+FATFileSystem fs("fs");
+InterruptIn mypin(D3);  // shinyei conf 1: GND   3:  5v   4: D3
+DigitalOut errorled(LED1);
+DigitalOut myled(LED3);
+Serial pc(USBTX, USBRX); // tx, rx
+
+
+//Shinyei variables
+Timer timer_Sh;
+double pm =0;
+int counter_pm;
+
+//SPEC variables
+int measure;
+int counter_spec;
+long concentration;
+long temperature;
+long humidity;
+
+
 
 // Ticker to take measurements repeatedly
 Ticker airTicker;
@@ -17,21 +42,46 @@ Ticker airTicker;
 // Interrupt flags
 bool flag_gas = true;
 
+
 // Callback function called by the tikcer 
 void flipFlags() {
     flag_gas = true;
     //printf("\r\nIn ticker callback\r\n");
 }
+
+// Callback functions for pwm read
+void on_fall() {
+    timer_Sh.start();
+    myled = mypin;
+}
+
+void on_rise() {
+    timer_Sh.stop();
+    myled = mypin;
+}
  
 int main() {
-
-    //printf("\r\nIn main\r\n");
+    printf("[System]  Hello...\r\n");
+    int error = 0;
+    errorled = 1;
+    myled = 1;
+    printf("[System]  Mounting the filesystem on \"/fs\"...   ");
+    error = fs.mount(&bd);
+    return_error(error);
+    printf("[System]  Creating csv labels...               ");
+    FILE* fd = fopen("/fs/samples.csv", "w");
+    errno_error(fd);
+    fprintf(fd, ",o3,so2,co,no2,IAQ,error,pm25,pressure,wind_Hum,wind_Tem,Sent\r\n");
+    printf("[System]  Running...\r\n");
+    fclose(fd);    
+ 
 
     // Some valiables
     char gasConc[8] = "";           // To store the gas concentration
     bool network_ok = false;        // To store the network status
 
-    SentiloServer sentiloServer = {"address", "providerId", "theToken"};
+
+    SentiloServer sentiloServer = {"http://sistemic.udea.edu.co:9091", "udeaProvider", "f4b92b05becd3b0b4d894ed78ffd468126a2622011a5a5aaf3165119c24431b7"};
 
     // Connect to the network
     nsapi_error_t con_st = netif->connect();
@@ -44,21 +94,90 @@ int main() {
 
     // Set and start the ticker
     airTicker.attach(&flipFlags, MEASUREMENT_PERIOD); // the address of the function to be attached and the interval
+
+	// Shinyei interrupts
+	mypin.rise(&on_rise);
+	mypin.fall(&on_fall);
     
     // spin in a main loop. flipper will interrupt it to call flip
     while(1) {
         //printf("\r\nIn main loop\r\n");
         if(flag_gas) {
             //printf("\r\nIn flag_gas\r\n");
-            getGasConcentration(gasConc);
-            //printf("The received CO: %s\r\n", gasConc);
-            if(network_ok){
-                sendObservation(sentiloServer, "udeaTemp", gasConc);
+            getGasConcentration(gasConc, &measure, &concentration, &temperature, &humidity, &counter_spec);
+            getDust(&pm,&counter_pm);
+            if(network_ok && counter >= 1){ // send to sentilo each 1 minutes...
+                std::ostringstream ss1;
+                std::ostringstream ss2;
+                std::ostringstream ss3;
+                std::ostringstream ss4;
+                ss1 << concentration;
+                ss2 << temperature;
+                ss3 << humidity;
+                ss4 << pm;
+                std::string val_spec(ss1.str());
+                std::string val_temp(ss2.str());
+                std::string val_hum(ss3.str());
+                std::string val_pm(ss4.str());
+                long s_o3= -9999;
+                long s_so2= -9999;
+                long s_co= -9999;
+                long s_no2= -9999;
+                long s_IAQ= -9999;
+                int conc_error = 0;
+                switch(measure) {
+                    case 0  : s_o3 = concentration;
+                    break;
+                    case 1  : s_so2 = concentration;
+                    break;
+                    case 2  : s_co = concentration;
+                    break;
+                    case 3  : s_no2 = concentration;
+                    break;
+                    case 4  : s_IAQ = concentration;
+                    break;
+                    default : conc_error = 1;}
+                sendObservation(sentiloServer, "spec", val_spec.c_str(),&ack);
+                sendObservation(sentiloServer, "temp", val_temp.c_str(),&ack);
+                sendObservation(sentiloServer, "hum", val_hum.c_str(),&ack);
+                sendObservation(sentiloServer, "pm", val_pm.c_str(),&ack);
+                printf("[System]  Appending file...\r\n");
+                // long %ld double %lf
+                // fprintf(fd, ",o3,so2,co,no2,IAQ,error,pm25,pressure,wind_Hum,wind_Tem,Sent\r\n");
+                fprintf(fd, "%ld,%ld,%ld,%ld,%ld,%ld,%d,%lf,,%ld,%ld,%d\r\n",samples,s_o3,s_so2,s_co,s_no2,s_IAQ,conc_error,pm,humidity,temperature,ack);
+                printf("[System]  File closed.\r\n");
+                fclose(fd);
+                errorled = (ack == 4);
+                counter = 0;
+                counter_pm = 0;
+                counter_spec = 0;
+                pm = 0;
+                concentration = 0;
+                temperature = 0;
+                humidity = 0;
+            }
+            else{
+                counter++;
             }
             flag_gas = false;
         }
         wait(0.2);
     }
+}
+
+/**
+* getDust calculate the concentration of Pm using shinyei sensor the PMW signal
+* conected to a digital input obtaining the  low state time in 30 secs
+*/
+void getDust(double *pm, int *counter){
+    timer_Sh.stop();
+    int timing= timer_Sh.read_ms();
+    double perc = (((double)timing)/30000)*100;
+    double pm_sample = (1.1*pow(perc,3) + 3.8*pow(perc,2) + 520*perc + 0.62)*0.283;
+    *pm =  (((*pm)*(*counter))+pm_sample)/((*counter)+1);
+    *counter = *counter + 1;
+    printf("[SHINYEI] low state: %d (%lf %%) = %lf  Avr = %lf pcs/L.\r\n", timing, perc, pm_sample, *pm);
+    timer_Sh.reset();
 }
 
 /**
@@ -68,7 +187,8 @@ int main() {
 *
 * @param[in] char* gasConc: the c-string(char*) to store the captured value
 */
-void getGasConcentration(char *gasCon) {
+
+void getGasConcentration(char *gasCon, int *measure, long *concentration, long *temperature, long *humidity, int *counter) {
     Serial specSensor(D1, D0);      // To communicate with the spec sensor
     //printf("\r\nIn fucking getGasConcentration!!!\r\n");
     char gasID[16];
@@ -83,23 +203,36 @@ void getGasConcentration(char *gasCon) {
     specSensor.scanf("%4s",  hum);
 
     if (strcmp(gasID, ID_03) == 0){
-        printf("O3: %s temp: %s hum: %s\r\n", gasCon, temp, hum);
+        printf("[SPEC]    O3: %s temp: %s hum: %s\r\n", gasCon, temp, hum);
+        *measure = 0;
     }
     else if (strcmp(gasID, ID_SO2) == 0){
-        printf("SO2: %s temp: %s hum: %s\r\n", gasCon, temp, hum);
+        printf("[SPEC]    SO2: %s temp: %s hum: %s\r\n", gasCon, temp, hum);
+        *measure = 1;
     } 
     else if (strcmp(gasID, ID_CO) == 0){
-        printf("CO: %s temp: %s hum: %s\r\n", gasCon, temp, hum);
+        printf("[SPEC]    CO: %s temp: %s hum: %s\r\n", gasCon, temp, hum);
+        *measure = 2;
     }
     else if (strcmp(gasID, ID_NO2) == 0){
-        printf("NO2: %s temp: %s hum: %s\r\n", gasCon, temp, hum);
+        printf("[SPEC]    NO2: %s temp: %s hum: %s\r\n", gasCon, temp, hum);
+        *measure = 3;
     } 
     else if (strcmp(gasID, ID_IAQ) == 0){
-        printf("IAQ: %s temp: %s hum: %s\r\n", gasCon, temp, hum);
+        printf("[SPEC]    IAQ: %s temp: %s hum: %s\r\n", gasCon, temp, hum);
+        *measure = 4;
     }
     else{
-        printf("SPEC sensor is not configurated");  
+        printf("[SPEC] SPEC sensor is not configurated");  
+        *measure = 5;
     }
+    if (*measure <5){
+        *concentration =  (((*concentration)*(*counter))+ my_long(gasCon))/((*counter)+1);
+        *temperature =  (((*temperature)*(*counter))+ my_long(temp))/((*counter)+1);
+        *humidity =  (((*humidity)*(*counter))+ my_long(hum))/((*counter)+1);
+        *counter = *counter + 1;
+    }
+
 
     // Remove the ',' at the end of the gasConc
     for(int i = 0; gasCon[i] != '\0'; i++) {
@@ -116,13 +249,7 @@ void getGasConcentration(char *gasCon) {
 * @param[in] HttpResponse* res: the response to be printed
 */
 void dump_response(HttpResponse* res) {
-    printf("Status: %d - %s\r\n", res->get_status_code(), res->get_status_message().c_str());
-
-    printf("Headers:\r\n");
-    for (size_t ix = 0; ix < res->get_headers_length(); ix++) {
-        printf("\t%s: %s\r\n", res->get_headers_fields()[ix]->c_str(), res->get_headers_values()[ix]->c_str());
-    }
-    printf("\r\nBody (%d bytes):\r\n\r\n%s\r\n", res->get_body_length(), res->get_body_as_string().c_str());
+    printf("[Network] Status: %d - %s\r\n", res->get_status_code(), res->get_status_message().c_str());
 }
 
 /**
@@ -132,7 +259,7 @@ void dump_response(HttpResponse* res) {
 * @param[in] std::string sensorID: the sensor ID
 * @param[in] std::string value: the value of the observation to be sent
 */
-void sendObservation(SentiloServer &sentiloServer, std::string sensorID, std::string value) {
+void sendObservation(SentiloServer &sentiloServer, std::string sensorID, std::string value, int *ack) {
     
     // Build the URL Request
     std::string reqURL (sentiloServer.address + "/data/" + sentiloServer.providerId + "/" + sensorID + "/" + value);
@@ -149,10 +276,10 @@ void sendObservation(SentiloServer &sentiloServer, std::string sensorID, std::st
         printf("HttpRequest failed (error code %d)\r\n", put_req->get_error());
         //return 1;
     }
-
-    printf("\r\n----- HTTP PUT response -----\r\n");
+    else{
+        *ack = *ack + 1;
+    }
     dump_response(put_res);
-
     delete put_req;
 }
 
--- source/airQuality.h
+++ source/airQuality.h
@@ -4,16 +4,25 @@
 
 #include "mbed.h"
 #include "http_request.h"
+#include "FATFileSystem.h"
+#include "SDBlockDevice.h"
 #include "EthernetInterface.h"
 #include <string>
+#include <math.h>
+#include <stdio.h>
+#include <sstream>
+#include <iostream>
+#include <errno.h>
+
 
 #define         ASCII_CR                    13
-#define         MEASUREMENT_PERIOD          10.0
+#define         MEASUREMENT_PERIOD          30.0
 #define         ID_03                       "092117010705,"
 #define         ID_SO2                      "081116050515,"
 #define         ID_CO                       "041117010433,"
 #define         ID_NO2                      "091117010115,"
 #define         ID_IAQ                      "081216010737,"
+#define         ENTER    13
 
 // A struct for the SentiloServer object
 typedef struct SentiloServerSt {
@@ -23,8 +32,43 @@ typedef struct SentiloServerSt {
 } SentiloServer;
 
 // Functions declaration
-void getGasConcentration(char *);
+void getGasConcentration(char *, int *measure, long *concentration, long *temperature, long *humidity, int *counter);
+void getDust(double *pm, int *counter);
 void dump_response(HttpResponse *);
-void sendObservation(SentiloServer &, std::string, std::string);
+void sendObservation(SentiloServer &, std::string, std::string, int *ack);
+
+// Storage error functions
+
+void return_error(int ret_val){
+  if (ret_val)
+    printf("Failure. %d\r\n", ret_val);
+  else
+    printf("done.\r\n");
+}
+
+void errno_error(void* ret_val){
+  if (ret_val == NULL)
+    printf(" Failure. %d \r\n", errno);
+  else
+    printf(" done.\r\n");
+}
+
+// long conversor
+
+long my_long(char *str){
+  long result=0;
+  long puiss=1;
+  while (('-' == (*str)) || ((*str) == '+'))
+    {
+      if (*str == '-')
+        puiss = puiss * -1;
+      str++;
+    }
+  while ((*str >= '0') && (*str <= '9'))
+    {
+      result = (result * 10) + ((*str) - '0');
+      str++;
+    }
+  return (result * puiss);}
 
 #endif
